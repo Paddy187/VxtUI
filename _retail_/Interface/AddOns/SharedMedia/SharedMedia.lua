a444a797d78fa0b3cc8c7785bb9d41eccac7516c
Paddy187 <xpaddy187@web.de>
2022-09-16 20:51:40 +0200

    Add files via upload

---
+++ _retail_/Interface/AddOns/SharedMedia/SharedMedia.lua
@@ -0,0 +1,34 @@
+-- tab size is 4
+-- registrations for media from the client itself belongs in LibSharedMedia-3.0
+
+local LSM = LibStub("LibSharedMedia-3.0")
+local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
+
+local MediaType_BACKGROUND = LSM.MediaType.BACKGROUND
+local MediaType_BORDER = LSM.MediaType.BORDER
+local MediaType_FONT = LSM.MediaType.FONT
+local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
+
+-- -----
+-- BACKGROUND
+-- -----
+
+
+-- -----
+--  BORDER
+-- ----
+
+
+-- -----
+--   FONT
+-- -----
+
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+
---
+++ _retail_/Interface/AddOns/SharedMedia_MyMedia/MyMedia.lua
@@ -0,0 +1,36 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+LSM:Register("background", "arrow_glow", [[Interface\Addons\SharedMedia_MyMedia\background\arrow_glow.tga]]) 
+LSM:Register("background", "NaowhArrow", [[Interface\Addons\SharedMedia_MyMedia\background\NaowhArrow.tga]]) 
+LSM:Register("background", "NaowhArrowLeft", [[Interface\Addons\SharedMedia_MyMedia\background\NaowhArrowLeft.tga]]) 
+LSM:Register("background", "NaowhArrowRight", [[Interface\Addons\SharedMedia_MyMedia\background\NaowhArrowRight.tga]]) 
+LSM:Register("background", "NaowhGlow", [[Interface\Addons\SharedMedia_MyMedia\background\NaowhGlow.tga]]) 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+LSM:Register("border", "bubu01", [[Interface\Addons\SharedMedia_MyMedia\border\bubu01.tga]]) 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "Vxt 00", [[Interface\Addons\SharedMedia_MyMedia\font\Vxt 00.ttf]]) 
+LSM:Register("font", "Vxt 01", [[Interface\Addons\SharedMedia_MyMedia\font\Vxt 01.ttf]]) 
+LSM:Register("font", "Vxt 02", [[Interface\Addons\SharedMedia_MyMedia\font\Vxt 02.ttf]]) 
+LSM:Register("font", "Vxt 03", [[Interface\Addons\SharedMedia_MyMedia\font\Vxt 03.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "mUI4", [[Interface\Addons\SharedMedia_MyMedia\statusbar\mUI4.tga]]) 
+LSM:Register("statusbar", "Vxt RB1", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Vxt RB1.tga]]) 
+LSM:Register("statusbar", "Vxt RB2", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Vxt RB2.tga]]) 
+LSM:Register("statusbar", "Vxt RB3", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Vxt RB3.tga]]) 
+LSM:Register("statusbar", "Vxt", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Vxt.tga]]) 
